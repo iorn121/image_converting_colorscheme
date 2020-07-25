5d9ed4eff5dc3a62a4e2f501d0afbf9b601f25b6
iorn121 <wmt.tkn.121@gmail.com>
2020-07-26 07:13:18 +0900

    最適なkを計算する

--- main.py
+++ main.py
@@ -12,11 +12,37 @@ for file in files:
     name = Path(file).stem
     path_w = "output/result.txt"
 
-    # 画像の読み込み
-    img=cv2.imread(file)
-    k=5
-    kimg, kimg_s=imcon.Kmeans(img,k)
+    ret_y=[]
+    ret_x=[]
 
-    #出力
-    cv2.imwrite("output/{}_k={}.jpg".format(name,k),kimg)
-    cv2.imwrite("output/{}_k={}_s.jpg".format(name,k),kimg_s)
+    for i in range(2,9):
+
+        # 画像の読み込み
+        img=cv2.imread(file)
+        k=i
+        ret, kimg, kimg_s=imcon.Kmeans(img,k)
+        ret_y+=[int(ret)]
+        ret_x+=[i]
+
+        #出力
+        cv2.imwrite("output/{}_k={}.jpg".format(name,k),kimg)
+        cv2.imwrite("output/{}_k={}_s.jpg".format(name,k),kimg_s)
+    
+    fig=plt.figure()
+    plt.plot(ret_x,ret_y)
+    plt.grid()
+    fig.savefig("output/{}_glaph.jpg".format(name))
+
+    a=(ret_y[6]-ret_y[0])/2
+    b=ret_y[0]-a
+
+    ans=2
+    seg=0
+
+    for i in range(1,6):
+        temp=a*np.log2(ret_x[i])+b-ret_y[i]
+        if seg<temp:
+            ans=i+2
+            seg=temp
+
+    print("best image for {} is k={}".format(name,ans))
