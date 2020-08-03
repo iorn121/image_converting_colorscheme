53ed63ae2fc457701fb4396bda45698fcfa86973
iorn121 <wmt.tkn.121@gmail.com>
2020-08-03 19:55:31 +0900

    ファイルの書き出し方法、tempフォルダの作成削除など

--- main.py
+++ main.py
@@ -4,13 +4,19 @@ import image_converting as imcon
 from pathlib import Path
 import os, glob
 import matplotlib.pyplot as plt
+from flask import Flask
+import re
+import shutil
 
 # ファイル取得
 files = glob.glob("./input/*.jpg")
 
 for file in files:
     name = Path(file).stem
-    path_w = "output/result.txt"
+    os.makedirs("./output/{}".format(name),exist_ok=True)
+    os.makedirs("./output/temp",exist_ok=True)
+    img=cv2.imread(file)
+    path_w = "./output/{}/result.txt".format(name)
 
     ret_y=[]
     ret_x=[]
@@ -18,20 +24,20 @@ for file in files:
     for i in range(2,9):
 
         # 画像の読み込み
-        img=cv2.imread(file)
         k=i
         ret, kimg, kimg_s=imcon.Kmeans(img,k)
         ret_y+=[int(ret)]
         ret_x+=[i]
 
         #出力
-        cv2.imwrite("output/{}_k={}.jpg".format(name,k),kimg)
-        cv2.imwrite("output/{}_k={}_s.jpg".format(name,k),kimg_s)
+        cv2.imwrite("./output/temp/{}_k={}.jpg".format(name,k),kimg)
+        cv2.imwrite("./output/temp/{}_k={}_s.jpg".format(name,k),kimg_s)
+
     
     fig=plt.figure()
     plt.plot(ret_x,ret_y)
     plt.grid()
-    fig.savefig("output/{}_glaph.jpg".format(name))
+    fig.savefig("./output/{}/{}_glaph.jpg".format(name,name))
 
     a=(ret_y[6]-ret_y[0])/2
     b=ret_y[0]-a
@@ -45,4 +51,7 @@ for file in files:
             ans=i+2
             seg=temp
 
-    print("best image for {} is k={}".format(name,ans))
+    new_path = shutil.move("./output/temp/{}_k={}.jpg".format(name,ans),"./output/{}/k={}.jpg".format(name,ans))
+    new_path_s = shutil.move("./output/temp/{}_k={}_s.jpg".format(name,ans),"./output/{}/result.jpg".format(name))
+
+    delete = shutil.rmtree("./output/temp")
