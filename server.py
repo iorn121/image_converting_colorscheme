d4efe2e3334b9f9010fb627c84e1656f156ac1ab
iorn121 <wmt.tkn.121@gmail.com>
2020-08-06 17:33:01 +0900

    error

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask import Flask, request, abort, render_template, request, redirect, url_for, send_from_directory
 import numpy as np
 import cv2
 import image_converting as imcon
@@ -24,9 +24,13 @@ def index():
 def send_js(filename):
     return send_from_directory(SAVE_DIR, filename)
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('index.html')
+
 @app.route('/upload', methods=['POST'])
 def upload():
-    if request.files['image']:
+    if request.files['image'] and request.form['clusterN']:
 
         #クラスター数の入力情報取得
         k=request.form.get('clusterN')
@@ -47,6 +51,8 @@ def upload():
         print("save", save_path)
 
         return redirect('/')
+    else:
+        abort(404)
 
 if __name__=="__main__":
     app.run(debug=True,host="0.0.0.0",port=5000,threaded=True)
