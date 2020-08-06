fef65989eaaf137eee2437d6c003fdc71bfacd1e
iorn121 <wmt.tkn.121@gmail.com>
2020-08-06 09:41:52 +0900

    new

--- server.py
+++ server.py
@@ -42,5 +42,5 @@ def upload():
         return redirect('/')
 
 if __name__=="__main__":
-    app.run(debug=True,host="0.0.0.0",port=5000,threaded=True)
+    app.run(debug=True,host="0.0.0.0",port=os.environ.get('PORT',5000),threaded=True)
 #http://127.0.0.1:5000/
