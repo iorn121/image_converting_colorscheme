3675b384537165125bb319ed44380b16530d3122
iorn121 <wmt.tkn.121@gmail.com>
2020-08-07 16:37:22 +0900

    e

--- server.py
+++ server.py
@@ -55,5 +55,5 @@ def upload():
         abort(404)
 
 if __name__=="__main__":
-    app.run(debug=True,host="0.0.0.0",port=5000,threaded=True)
+    app.run(debug=True,threaded=True)
 #http://127.0.0.1:5000/
